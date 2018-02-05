76e90adc0874294035e9f802ce13f102b991e6cf
Chris Van Law <chris@chrisvanlaw.com>
2018-02-05 08:27:39 -0700

    Add api shell.

---
+++ api/wedding-api/Controllers/RsvpController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wedding_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RsvpController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]RsvpModel rsvpModel)
+        {
+        }
+    }
+
+    public class RsvpModel
+    {
+        public List<GuestModel> Guests { get; set; }
+    }
+
+    public class GuestModel
+    {
+        public string GuestName { get; set; }
+
+        public MealSelection MealSelection { get; set; }
+    }
+
+    public enum MealSelection
+    {
+        Undefined = 0,
+        Beef,
+        Fish,
+        Chicken,
+        Veggie,
+        Kids
+    }
+}
